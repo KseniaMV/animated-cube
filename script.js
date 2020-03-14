7824d9ae9075e6eca390c0cf5954a072347f8650
Ksenia <48721061+KseniaMV@users.noreply.github.com>
2020-03-14 14:15:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let cube = document.querySelector(".cube");
+document.body.addEventListener("click", function(){
+        cube.style.transform += "rotateY(-80deg)";
+        cube.style.transition += "3s";
+});
+    
